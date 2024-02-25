LMODroid/platform_packages_apps_Updater

Update gradle scripts for android 14
Change-Id: I962a58af4618f3ecdd338cc41c55d92cadaa1ff2

app/build.gradle.kts
@@ -30,12 +30,13 @@ val keystoreProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
+    namespace = "com.libremobileos.updater"
 
     defaultConfig {
         applicationId = "com.libremobileos.updater"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }

rising-source-mod/android_packages_apps_Updater

Signed-off-by: Alvin Francis <nivlafx@gmail.com>

app/build.gradle.kts
@@ -30,12 +30,13 @@ val keystoreProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
+    namespace = "com.rising.updater"
 
     defaultConfig {
         applicationId = "com.rising.updater"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }